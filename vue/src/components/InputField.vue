0878c5bbbf9eac6457cf5fc11dea87f195a55d7f
Thomas Carr <Thomas.Ryan.Carr@gmail.com>
2022-08-09 12:42:42 -0400

    Front end User Messages Added

--- vue/src/components/InputField.vue
+++ vue/src/components/InputField.vue
@@ -15,78 +15,28 @@ data()
    {
     return {
       msg: {
-        id: this.$store.state.messages.length + 1,
+        message_id: "",
+        user_id: "",
         body: "",
         sender: "student",
         type: "text",
         link:""
       },
-      msgWithUsername: {
-        id: 4,
-        body: "",
-        sender: "bot",
-        type: "text",
-        link:""
-      },
+      
     }
   },
-  // methods: 
-  // {
-  //   addMessage() {
-  //     const message = this.msg
-  //     this.$store.commit("ADD_MESSAGE", message);
-  //     if(this.$store.state.messages.length === 3){
-  //       this.msgWithUsername.body = "Hi, " + this.msg.body + "!";
-  //       this.$store.commit("ADD_MESSAGE", this.msgWithUsername);
-  //       chatService.getAllTopics().then(response =>{
-  //         const botMessage = {
-  //                 id: this.$store.state.messages.length + 1,
-  //                 body: response.data.body,
-  //                 sender: "bot",
-  //                 type: response.data.type,
-  //                 link: response.data.link
-  //         }
-  //       this.$store.commit("ADD_MESSAGE", botMessage);
-        
-  //       });
+   methods:{
+     addMessage() {
+       chatService.sendMessage(this.msg).then(response => {
+         response.data.forEach(message => {
+           this.$store.commit("ADD_MESSAGE", message)
+         })
+       });
+     }
 
-  //     } else {
-  //     chatService.sendMessage(this.msg).then(response =>{
-  //         response.data.forEach(msgBody => {
-  //           const botMessage = {
-  //               id: this.$store.state.messages.length + 1,
-  //               body: msgBody.body,
-  //               sender: "bot",
-  //               type: msgBody.type,
-  //               link: msgBody.link
-  //           }
-  //           this.$store.commit("ADD_MESSAGE", botMessage)
-  //         });
-  //       });
-  //       this.msg.id = this.$store.state.messages.length +1
-  //     }
-  //     console.log("We're getting a student message");
-  //     this.msg = {
-        
-  //       id: this.$store.state.messages.length + 1,
-  //       body: "",
-  //       sender: "student",
-  //       type: "text",
-  //       link:""
-  //     }
-     
-  //   },
-  //   //need an "if the message index is 3 , or messages.length = 3 , send list of topics
-  //   //Or if the message.body != topic, return list of topics
-    
-  //   ///this doesn't work
-  //   createListOfTopics(){
-  //     chatService.getAllTopics().then( response => {
-  //           this.topicsList = response.data;
-  //   })
   
-  // }
-  //},
+ 
+  },
   created() {
     chatService.getInitialMessages().then(response => {
        response.data.forEach(botMessage => {
