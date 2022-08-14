fce222ffb289aab28d5a4878073c49aefdd49b98
Caitlin Naber <caitlin.naber@gmail.com>
2022-08-14 00:25:19 +0000

    Revert "updates to email form to show/hide form on button click"
    This reverts commit a096f3484a290bb7ce884efdbc7ff75a38676750

--- vue/src/components/NewEmail.vue
+++ vue/src/components/NewEmail.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="emailContainer">
-    <button @click="toggleForm()" id="showEmailForm" class="showEmailForm">Email</button>
-    <div id="emailForm" class="hidden" ref="emailForm">
-      <form ref="form" @submit.prevent="sendEmail">
-        <label>Name</label>
-        <input type="text" name="name">
-        <label>Email</label>
-        <input type="email" name="email">
-        <label>Message</label>
-        <textarea name="message"></textarea>
-        <input type="submit" value="Send">
-      </form>
-    </div>
-  </div>
+  <form ref="form" @submit.prevent="sendEmail">
+    <label>Name</label>
+    <input type="text" name="name">
+    <label>Email</label>
+    <input type="email" name="email">
+    <label>Message</label>
+    <textarea name="message"></textarea>
+    <input type="submit" value="Send">
+  </form>
 </template>
 
 <script>
@@ -28,22 +23,6 @@ export default {
             console.log('FAILED...', error.text);
         });
         this.$refs.form.reset();
-    },
-    // showForm() {
-    //   var x = document.getElementById("emailForm");
-    //   if (x.style.display === "none") {
-    //     x.style.display = "block";
-    //   } 
-    // },
-    toggleForm() {
-      let emailForm = this.$refs.emailForm;
-      if (emailForm.classList.contains('hidden')) {
-        emailForm.classList.add('visible');
-        emailForm.classList.remove('hidden');
-      } else {
-        emailForm.classList.add('hidden');
-        emailForm.classList.remove('visible');
-      }
     }
   }
 }
@@ -55,18 +34,15 @@ export default {
     margin-left: 20%;
     margin: 8%;
 }
-
 .emailContainer{
     display: flex;
     flex-direction: column;
     align-items:center
 }
-
 label {
   float: left;
   margin-left: 4%;
 }
-
 input[type=text], [type=email], textarea {
   width: 80%;
   padding: 10px;
@@ -78,6 +54,7 @@ input[type=text], [type=email], textarea {
   resize: vertical;
   background-color: #EBF2F0;
   margin-left: 4%;
+  
 }
 
 input[type=submit] {
@@ -99,20 +76,14 @@ input[type=submit] {
   height: 36px;
   margin: 8%;
   margin-left: 20%;
+ 
 }
 
 input[type=submit]:hover {
  transform: scale(1.02);
   background-color: #2B57F1;
   color: white;
-}
-
-.hidden {
-  display: none;
-}
-
-.visible {
-  display: block;
+ 
 }
 
 </style>
