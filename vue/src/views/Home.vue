e3652ac1b0979197da502f617d17d35f38e2e1ee
Caitlin Naber <caitlin.naber@gmail.com>
2022-08-14 00:26:56 +0000

    Revert "syntax changes"
    This reverts commit 0f010793a3856fe18f83b215a95d6da434a6aea2

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -78,7 +78,7 @@ font-family: 'Roboto Mono', monospace;
 }
 
 .chat-bot {
-  height: 90%;
+  height: 90vh;
   grid-area: chat;
   overflow-y: auto;
 }
@@ -142,8 +142,7 @@ button:hover{
 #happy{
     background-color: #002A42;
     vertical-align: middle;
-    border-radius: 50%;
-    justify-content: center;
+    border-radius: 50%;justify-content: center;
     padding:10%;
     margin-left: 22%;
 }
