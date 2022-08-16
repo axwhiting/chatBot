917ae3a41dd9e83136278ba919e9fcc6f2a53884
Ashley Whiting <98121650+axwhiting@users.noreply.github.com>
2022-08-15 21:11:34 -0400

    pushing with message animation

--- vue/src/components/ChatBot.vue
+++ vue/src/components/ChatBot.vue
@@ -172,4 +172,21 @@ label {
 .mc-input {
   margin: 10px;
 }
+div.bot{
+	animation: slide-in-elliptic-right-fwd 0.7s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+
+}
+@keyframes slide-in-elliptic-right-fwd {
+  0% {
+    transform: translateX(800px) rotateY(-30deg) scale(0);
+    transform-origin: -100% 50%;
+    opacity: 0;
+  }
+  100% {
+    transform: translateX(0) rotateY(0) scale(1);
+    transform-origin: -1800px 50%;
+    opacity: 1;
+  }
+}
+
 </style>
