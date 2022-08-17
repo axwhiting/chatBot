b5401f6ecd4d33d00004389314097a9320c5c9dd
Ashley Whiting <98121650+axwhiting@users.noreply.github.com>
2022-08-17 09:53:19 -0400

    pushing with speech recognition not functioning

---
+++ vue/src/components/Speech.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input type="text" id="spokenText" />
+    <button id="speakyButton" disabled>Press and Say</button>
+    <p id="notices"></p>
+    </div>
+</template>
+
+<script src="speech-to-text.js"></script>
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
