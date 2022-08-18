9f2bbe83d6787c1d2f0a887712cb4afd7374591b
Caitlin Naber <caitlin.naber@gmail.com>
2022-08-18 09:49:27 -0400

    added quote button

--- vue/src/components/NavBar.vue
+++ vue/src/components/NavBar.vue
@@ -6,6 +6,7 @@
         <div class="menu">
           <button @click="sendClick('About Codee Chatbot')" id="about" class="about button">About</button>
           <button @click="sendClick('Help for Codee Chatbot')" id="help" class="help button">Help</button>
+          <button @click="sendClick('Motivational Quote')" id="motivat" class="motivate button">Motivate</button>
           <button @click="sendClick('Get meme')" id="meme" class="meme button">Meme</button>
           <button @click="sendClick('Interview Question')" id="interview" class="interview button">Interview</button>
           <button @click="saySomething()" id="speech" class="speech button">Talk to me</button>
